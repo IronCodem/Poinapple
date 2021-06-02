ce82fcc37a044cd2418967d789dd7b587e523cc5
iop3 <iop3.is.epic@gmail.com>
2021-06-02 08:29:53 -0700

    ee

--- recipe/recipe.java
+++ recipe/recipe.java
@@ -1,10 +1,10 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Recipe of obtaining the perfect pinnapple");
-    System.out.println("Get a pinnapple");
-    System.out.println("Slice it up");
-    System.out.println("Done");
-    System.out.println("Eat it");
+    System.out.println("1.Get a pinnapple");
+    System.out.println("2.Slice it up");
+    System.out.println("3.Done");
+    System.out.println("4.Eat it");
   }
 }
 // lul
