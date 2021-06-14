f9d7cb66024e0f18a25409b519f1e7282a35ee59
Yashas Shah <79524299+Tr1angular@users.noreply.github.com>
2021-06-14 10:38:30 -0400

    typescript

---
+++ recipe/recipe.ts
@@ -0,0 +1,14 @@
+interface Recipe {
+  1, 2, 3, 4: string;
+}
+
+function greeter(Recipe: recipe) {
+  return "Recipe of obtaining the perfect pinnapple, " + recipe.1 + ", " + recipe.2 + ", "; + recipe.3 + ", " + recipe.4 + ".";
+}
+
+let recipe = {
+  1: "1. Get a pinnapple",
+  2: "2. Slice it up"
+  3: "3. Done"
+  4. "4. Eat it"
+};
