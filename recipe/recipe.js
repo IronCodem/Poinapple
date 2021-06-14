adfde92df33673bc6c43e84a30d42aa20d267229
Yashas Shah <79524299+Tr1angular@users.noreply.github.com>
2021-06-14 10:39:00 -0400

    javascript

--- recipe/recipe.js
+++ recipe/recipe.js
@@ -1,5 +1,7 @@
-console.log("Recipe of obtaining the perfect pinnapple");
-console.log("1.Get a pinnapple");
-console.log("2.Slice it up");
-console.log("3.Done");
-console.log("4.Eat it");
+console.log("Recipe of obtaining the perfect pinnapple:");
+
+
+console.log("1. Get a pinnapple");
+console.log("2. Slice it up");
+console.log("3. Done");
+console.log("4. Eat it");
